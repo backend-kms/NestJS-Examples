1b075e47bce10f8ec7f633dcd99d400abbf0d079
backend-kms <minsu6424@naver.com>
2025-01-17 13:36:01 +0900

    fix: @EntityRepository()가 typeORM 0.3X 이후부터 사용중지 -> DataSource를 통해 생성 및 관리로 변경

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -1,5 +1,13 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { Board } from './board.entity';
+import { Injectable } from '@nestjs/common';
 
-@EntityRepository(Board)
-export class BoardRepository extends Repository<Board> {}
+// @EntityRepository(Board)
+// export class BoardRepository extends Repository<Board> {}
+
+@Injectable()
+export class BoardRepository extends Repository<Board> {
+  constructor(private readonly dataSource: DataSource) {
+    super(Board, dataSource.createEntityManager());
+  }
+}
